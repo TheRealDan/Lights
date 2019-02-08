df9a89903f7df568d4c676f2be6c5e5f3d3a8efb
TheRealDan <thedanielhailwood@gmail.com>
2019-02-08 14:50:21 +1300

    Programmer tidy up since it had a lot of unused methods

--- core/src/me/therealdan/lights/programmer/Programmer.java
+++ core/src/me/therealdan/lights/programmer/Programmer.java
@@ -8,13 +8,11 @@ import me.therealdan.lights.ui.views.live.ui.SequencesUI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Programmer {
 
     private static List<Fixture> selectedFixtures = new ArrayList<>();
-    private static LinkedHashMap<Channel.Type, List<Integer>> selectedParameters = new LinkedHashMap<>();
 
     private static Sequence sequence = null;
 
@@ -57,12 +55,6 @@ public class Programmer {
             select(fixture);
     }
 
-    public static void select(Channel.Type channelType, int parameter) {
-        if (!selectedParameters.containsKey(channelType))
-            selectedParameters.put(channelType, new ArrayList<>());
-        selectedParameters.get(channelType).add(parameter);
-    }
-
     public static void deselect(Fixture fixture) {
         selectedFixtures.remove(fixture);
     }
@@ -72,44 +64,12 @@ public class Programmer {
             deselect(fixture);
     }
 
-    public static void deselect(Channel.Type channelType) {
-        selectedParameters.get(channelType).clear();
-    }
-
-    public static void deselect(Channel.Type channelType, int parameter) {
-        List<Integer> selectedParameters = getSelectedParameters(channelType);
-        deselect(channelType);
-
-        for (int selectedParameter : selectedParameters)
-            if (selectedParameter != parameter)
-                select(channelType, selectedParameter);
-    }
-
-    public static void deselectAllFixtures() {
-        for (Fixture fixture : getSelectedFixtures())
-            deselect(fixture);
-    }
-
-    public static void deselectAllGroups() {
-        for (Group group : PatchUI.groups())
-            deselect(group);
-    }
-
-    public static void deselectAllParameters() {
-        for (Channel.Type channelType : Channel.Type.values())
-            selectedParameters.get(channelType).clear();
-    }
-
     public static float getValue(int address) {
         return sequence.getActiveFrame().getValue(address);
     }
 
     public static boolean hasValue(int address) {
         return getSequence().getActiveFrame().hasValue(address);
-    }
-
-    public static boolean hasFixturesSelected() {
-        return selectedFixtures.size() > 0;
     }
 
     public static boolean isSelected(Fixture fixture) {
@@ -124,14 +84,6 @@ public class Programmer {
         return true;
     }
 
-    public static boolean isSelected(Channel.Type channelType) {
-        return getSelectedParameters(channelType).size() > 0;
-    }
-
-    public static boolean isSelected(Channel.Type channelType, int parameter) {
-        return getSelectedParameters(channelType).contains(parameter);
-    }
-
     public static Sequence getSequence() {
         if (sequence == null) clear();
         return sequence;
@@ -139,35 +91,5 @@ public class Programmer {
 
     public static List<Fixture> getSelectedFixtures() {
         return new ArrayList<>(selectedFixtures);
-    }
-
-    public static List<Channel.Type> getSelectedChannelTypes() {
-        List<Channel.Type> channelTypes = new ArrayList<>();
-        for (Channel.Type channelType : Channel.Type.values())
-            if (getSelectedParameters(channelType).size() > 0)
-                channelTypes.add(channelType);
-        return channelTypes;
-    }
-
-    public static List<Integer> getSelectedParameters(Channel.Type channelType) {
-        if (!selectedParameters.containsKey(channelType))
-            selectedParameters.put(channelType, new ArrayList<>());
-        return selectedParameters.get(channelType);
-    }
-
-    public static LinkedHashMap<Channel.Type, Integer> getAvailableParameters() {
-        LinkedHashMap<Channel.Type, Integer> parameters = new LinkedHashMap<>();
-
-        for (Fixture fixture : getSelectedFixtures()) {
-            LinkedHashMap<Channel.Type, Integer> tempParameters = new LinkedHashMap<>();
-            for (Channel channel : fixture.channels())
-                tempParameters.put(channel.getType(), tempParameters.getOrDefault(channel.getType(), 0) + 1);
-
-            for (Channel.Type channelType : tempParameters.keySet())
-                if (tempParameters.get(channelType) > parameters.getOrDefault(channelType, 0))
-                    parameters.put(channelType, tempParameters.get(channelType));
-        }
-
-        return parameters;
     }
 }

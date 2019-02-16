9c6e430a3d275abff1839ca7276dd48b17014127
TheRealDan <thedanielhailwood@gmail.com>
2019-02-17 12:38:43 +1300

    Implemented CENTER text position for GroupsUI

--- core/src/me/therealdan/lights/ui/views/live/ui/GroupsUI.java
+++ core/src/me/therealdan/lights/ui/views/live/ui/GroupsUI.java
@@ -6,6 +6,7 @@ import me.therealdan.lights.LightsCore;
 import me.therealdan.lights.fixtures.Group;
 import me.therealdan.lights.programmer.Programmer;
 import me.therealdan.lights.renderer.Renderer;
+import me.therealdan.lights.renderer.Task;
 import me.therealdan.lights.ui.views.Live;
 import me.therealdan.lights.util.Util;
 
@@ -31,12 +32,12 @@ public class GroupsUI implements UI {
         float y = getY();
         float width = getWidth();
 
-        Util.box(renderer, x, y, width, cellHeight, LightsCore.DARK_BLUE, setWidth(renderer, "Groups"));
+        Util.box(renderer, x, y, width, cellHeight, LightsCore.DARK_BLUE, setWidth(renderer, "Groups"), Task.TextPosition.CENTER);
         drag(x, y, width, cellHeight);
         y -= cellHeight;
 
         for (Group group : PatchUI.groups()) {
-            Util.box(renderer, x, y, cellSize, cellSize, Programmer.isSelected(group) ? LightsCore.DARK_RED : LightsCore.medium(), setWidth(renderer, group.getName()));
+            Util.box(renderer, x, y, cellSize, cellSize, Programmer.isSelected(group) ? LightsCore.DARK_RED : LightsCore.medium(), setWidth(renderer, group.getName()), Task.TextPosition.CENTER);
             if (Util.containsMouse(x, y, cellSize, cellSize) && canInteract()) {
                 interacted = true;
                 if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && LightsCore.actionReady(500)) {

66fb3dfaa44e141bc361fccf969143cdd13a6f89
TheRealDan <thedanielhailwood@gmail.com>
2019-02-22 17:41:01 +1300

    Removed previous loop() because the new one works great

--- Arduino/Lights/Lights.ino
+++ Arduino/Lights/Lights.ino
@@ -52,37 +52,6 @@ void loop() {
   }
 }
 
-void loopV1() {
-  if (rx.available() > 0) {
-    incoming = rx.read();
-
-    switch(incoming) {
-      case 32:
-        if (dmxBuffer.length() > 3) {
-          address = dmxBuffer.substring(0, 3).toInt();
-          value = dmxBuffer.substring(3, min(dmxBuffer.length(), 6)).toInt();
-          dmx(address, value);
-        }
-        dmxBuffer = "";
-        break;
-
-      case 48:
-      case 49:
-      case 50:
-      case 51:
-      case 52:
-      case 53:
-      case 54:
-      case 55:
-      case 56:
-      case 57:
-        c = incoming;
-        dmxBuffer.concat(c);
-        break;
-    }
-  }
-}
-
 void dmx(int address, int value) {
   if (address > 512) address = 512;
   if (address < 1) address = 1;

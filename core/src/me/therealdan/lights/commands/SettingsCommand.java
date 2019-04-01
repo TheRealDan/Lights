9a3ad53fdf6929fbaafc8ff0acd38d622c4ad1e1
Daniel <daniel@borndigital.co.nz>
2019-04-01 17:09:10 +1300

    Implemented new Settings system Settings command now dynamically supports all settings

--- core/src/me/therealdan/lights/commands/SettingsCommand.java
+++ core/src/me/therealdan/lights/commands/SettingsCommand.java
@@ -1,6 +1,6 @@
 package me.therealdan.lights.commands;
 
-import me.therealdan.lights.dmx.Output;
+import me.therealdan.lights.settings.Setting;
 import me.therealdan.lights.ui.views.live.ui.ConsoleUI;
 
 public class SettingsCommand implements Command {
@@ -10,41 +10,17 @@ public class SettingsCommand implements Command {
         if (!command.equalsIgnoreCase(getCommand())) return false;
 
         if (args.length > 1) {
-            if (args[0].equalsIgnoreCase("interval")) {
-                long value = Long.parseLong(args[1]);
-                Output.INTERVAL = value;
-                ConsoleUI.print("Set interval to: " + value);
-                return true;
-            } else if (args[0].equalsIgnoreCase("connection_wait")) {
-                long value = Long.parseLong(args[1]);
-                Output.CONNECTION_WAIT = value;
-                ConsoleUI.print("Set connection wait to: " + value);
-                return true;
-            } else if (args[0].equalsIgnoreCase("new_read_timeout")) {
-                int value = Integer.parseInt(args[1]);
-                Output.NEW_READ_TIMEOUT = value;
-                ConsoleUI.print("Set new read timeout to: " + value);
-                return true;
-            } else if (args[0].equalsIgnoreCase("new_write_timeout")) {
-                int value = Integer.parseInt(args[1]);
-                Output.NEW_WRITE_TIMEOUT = value;
-                ConsoleUI.print("Set new write timeout to: " + value);
-                return true;
-            } else if (args[0].equalsIgnoreCase("channels_per_send")) {
-                int value = Integer.parseInt(args[1]);
-                Output.CHANNELS_PER_SEND = value;
-                ConsoleUI.print("Set channels per send to: " + value);
+            Setting setting = Setting.byName(args[0]);
+            if (setting != null) {
+                setting.setValue(args[1]);
+
+                ConsoleUI.print(setting.getName() + " set to: " + setting.getValue());
                 return true;
             }
         }
 
-        ConsoleUI.print(
-                "settings interval [long]",
-                "settings connection_wait [long]",
-                "settings new_read_timeout [int]",
-                "settings new_write_timeout [int]",
-                "settings channels_per_send [int]"
-        );
+        for (Setting setting : Setting.settings())
+            ConsoleUI.print("settings " + setting.getName() + " [" + setting.getType().toString() + "]");
 
         return true;
     }

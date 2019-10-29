04a5eaada7c67ef760ac4d17bbc0713ffbbcfd29
TheRealDan <thedanielhailwood@gmail.com>
2019-10-30 07:57:16 +1300

    using Panel.CELL_HEIGHT for ActiveSequencesPanel

--- core/src/dev/therealdan/lights/panels/panels/ActiveSequencesPanel.java
+++ core/src/dev/therealdan/lights/panels/panels/ActiveSequencesPanel.java
@@ -3,10 +3,10 @@ package dev.therealdan.lights.panels.panels;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import dev.therealdan.lights.main.Lights;
+import dev.therealdan.lights.panels.Panel;
 import dev.therealdan.lights.programmer.Sequence;
 import dev.therealdan.lights.renderer.Renderer;
 import dev.therealdan.lights.renderer.Task;
-import dev.therealdan.lights.panels.Panel;
 import dev.therealdan.lights.ui.UIHandler;
 
 public class ActiveSequencesPanel implements Panel {
@@ -16,35 +16,33 @@ public class ActiveSequencesPanel implements Panel {
         if (containsMouse()) UIHandler.setSection(UIHandler.Section.ACTIVE_SEQUENCES);
         boolean interacted = false;
 
-        float cellHeight = 30;
-
         float x = getX();
         float y = getY();
         float width = getWidth();
 
-        renderer.box(x, y, width, cellHeight, Lights.color.DARK_BLUE, setWidth(renderer, getFriendlyName()), Task.TextPosition.CENTER);
-        drag(x, y, width, cellHeight);
-        y -= cellHeight;
+        renderer.box(x, y, width, Panel.CELL_HEIGHT, Lights.color.DARK_BLUE, setWidth(renderer, getFriendlyName()), Task.TextPosition.CENTER);
+        drag(x, y, width, Panel.CELL_HEIGHT);
+        y -= Panel.CELL_HEIGHT;
 
         for (int priority = 0; priority <= UIHandler.getTopPriority(); priority++) {
             Sequence sequence = UIHandler.getSequence(priority);
             if (sequence == null) continue;
             float priorityWidth = renderer.getWidth(Integer.toString(priority)) + 10;
-            renderer.box(x, y, priorityWidth, cellHeight, Lights.color.MEDIUM, Integer.toString(priority));
-            if (Lights.mouse.contains(x, y, priorityWidth, cellHeight) && canInteract()) {
+            renderer.box(x, y, priorityWidth, Panel.CELL_HEIGHT, Lights.color.MEDIUM, Integer.toString(priority));
+            if (Lights.mouse.contains(x, y, priorityWidth, Panel.CELL_HEIGHT) && canInteract()) {
                 interacted = true;
                 if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && Lights.mouse.leftReady(500)) {
                     UIHandler.clearSequence(priority);
                 }
             }
-            renderer.box(x + priorityWidth, y, width - priorityWidth, cellHeight, Lights.color.MEDIUM, sequence.getName());
-            if (Lights.mouse.contains(x, y, width - priorityWidth, cellHeight) && canInteract()) {
+            renderer.box(x + priorityWidth, y, width - priorityWidth, Panel.CELL_HEIGHT, Lights.color.MEDIUM, sequence.getName());
+            if (Lights.mouse.contains(x, y, width - priorityWidth, Panel.CELL_HEIGHT) && canInteract()) {
                 interacted = true;
                 if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && Lights.mouse.leftReady(500)) {
                     UIHandler.clearSequence(priority);
                 }
             }
-            y -= cellHeight;
+            y -= Panel.CELL_HEIGHT;
 
             if (width - priorityWidth < renderer.getWidth(sequence.getName()) + 10)
                 setWidth(width + 1);

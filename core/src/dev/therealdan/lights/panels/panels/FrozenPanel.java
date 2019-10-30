1b1d217b44fd563ca34b6e3a0ecd8b0d07726ff2
TheRealDan <thedanielhailwood@gmail.com>
2019-10-31 07:26:38 +1300

    updated FrozenPanel with new Panel implementation

--- core/src/dev/therealdan/lights/panels/panels/FrozenPanel.java
+++ core/src/dev/therealdan/lights/panels/panels/FrozenPanel.java
@@ -1,43 +1,44 @@
 package dev.therealdan.lights.panels.panels;
 
 import dev.therealdan.lights.main.Lights;
+import dev.therealdan.lights.panels.Panel;
 import dev.therealdan.lights.renderer.Renderer;
 import dev.therealdan.lights.renderer.Task;
-import dev.therealdan.lights.panels.Panel;
 import dev.therealdan.lights.ui.UIHandler;
 
 public class FrozenPanel implements Panel {
 
+    public FrozenPanel() {
+        setHeight(Panel.CELL_HEIGHT * 2);
+    }
+
     @Override
-    public boolean draw(Renderer renderer, float X, float Y, float WIDTH, float HEIGHT) {
-        if (containsMouse()) UIHandler.setSection(UIHandler.Section.FROZEN);
-        boolean interacted = false;
-
-        if (!Lights.output.isFrozen()) return interacted;
-
-        float cellHeight = 30;
-
-        float x = getX();
-        float y = getY();
-
-        float width = getWidth();
-
-        renderer.box(x, y, width, cellHeight,
-                System.currentTimeMillis() % 1000 > 500 ? Lights.color.DARK_BLUE : Lights.color.RED,
-                System.currentTimeMillis() % 1000 > 500 ? Lights.color.TEXT : Lights.color.BLACK,
+    public boolean drawMenuBar(Renderer renderer, float x, float y, float width, float height, float menuIconWidth, float menuIconHeight, float spacing) {
+        boolean flash = System.currentTimeMillis() % 1000 > 500;
+        renderer.box(x, y, width, height,
+                flash ? Lights.color.DARK_BLUE : Lights.color.RED,
+                flash ? Lights.color.TEXT : Lights.color.BLACK,
                 setWidth(renderer, "OUTPUT FROZEN"), Task.TextPosition.CENTER);
-        drag(x, y, width, cellHeight);
-        y -= cellHeight;
+        drag(x, y, width, height);
+        return false;
+    }
 
-        renderer.box(x, y, width, cellHeight, Lights.color.MEDIUM, setWidth(renderer, "Press Escape to unfreeze"), Task.TextPosition.CENTER);
-        y -= cellHeight;
+    @Override
+    public boolean drawContent(Renderer renderer, float x, float y, float width, float height) {
+        if (containsMouse()) UIHandler.setSection(UIHandler.Section.FROZEN);
 
-        setHeightBasedOnY(y);
-        return interacted;
+        renderer.box(x, y, width, height, Lights.color.MEDIUM, setWidth(renderer, "Press Escape to unfreeze"), Task.TextPosition.CENTER);
+        drag(x, y, width, height);
+        return false;
     }
 
     @Override
     public boolean ignoreVisibilityUI() {
         return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return Lights.output.isFrozen();
     }
 }

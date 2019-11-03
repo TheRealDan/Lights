4573bab60b13bc287a52fbb4ce5fa77785fa0ed0
TheRealDan <thedanielhailwood@gmail.com>
2019-11-03 15:51:22 +1300

    removed Add New Button button from ButtonsPanel, added AddButtonIcon to ButtonsPanel, based button size on Panel size and buttonsPerRow

---
+++ core/src/dev/therealdan/lights/panels/menuicons/AddButtonIcon.java
@@ -0,0 +1,25 @@
+package dev.therealdan.lights.panels.menuicons;
+
+import dev.therealdan.lights.controllers.Button;
+import dev.therealdan.lights.main.Lights;
+import dev.therealdan.lights.panels.MenuIcon;
+import dev.therealdan.lights.panels.Panel;
+import dev.therealdan.lights.renderer.Renderer;
+import dev.therealdan.lights.renderer.Task;
+
+public class AddButtonIcon implements MenuIcon {
+
+    @Override
+    public boolean click(Panel panel) {
+        Button.add(new Button("New Button", Lights.color.MEDIUM));
+        return true;
+    }
+
+    @Override
+    public boolean draw(Renderer renderer, float x, float y, float width, float height, int index, boolean hover, boolean click) {
+        float offset = 4;
+        renderer.queue(new Task(x + offset, y - height / 2).line(x + width - offset, y - height / 2).setColor(hover ? Lights.color.GREEN : Lights.color.WHITE));
+        renderer.queue(new Task(x + width / 2, y - offset).line(x + width / 2, y - height + offset).setColor(hover ? Lights.color.GREEN : Lights.color.WHITE));
+        return false;
+    }
+}
--- core/src/dev/therealdan/lights/panels/panels/ButtonsPanel.java
+++ core/src/dev/therealdan/lights/panels/panels/ButtonsPanel.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import dev.therealdan.lights.controllers.Button;
 import dev.therealdan.lights.main.Lights;
 import dev.therealdan.lights.panels.Panel;
+import dev.therealdan.lights.panels.menuicons.AddButtonIcon;
 import dev.therealdan.lights.panels.menuicons.CloseIcon;
 import dev.therealdan.lights.renderer.Renderer;
 import dev.therealdan.lights.renderer.Task;
@@ -15,61 +16,51 @@ public class ButtonsPanel implements Panel {
 
     public ButtonsPanel() {
         register(new CloseIcon());
+        register(new AddButtonIcon());
 
         setWidth(800);
+        setHeight(200);
     }
 
     @Override
     public boolean drawContent(Renderer renderer, float x, float y, float width, float height, boolean interacted) {
-        float size = getWidth() / getButtonsPerRow();
-
         ButtonEditorPanel buttonEditor = (ButtonEditorPanel) PanelHandler.byName("ButtonEditor");
 
         int topPositionToDisplay = Button.getTopPosition();
-        if (buttonEditor.isEditing()) {
-            while (topPositionToDisplay % getButtonsPerRow() != 0) topPositionToDisplay++;
-            topPositionToDisplay += getButtonsPerRow();
-        }
+        while (topPositionToDisplay % getButtonsPerRow() != 0) topPositionToDisplay++;
+        if (buttonEditor.isEditing()) topPositionToDisplay += getButtonsPerRow();
+
+        float buttonWidth = width / getButtonsPerRow();
+        float buttonHeight = height / (topPositionToDisplay / getButtonsPerRow());
 
         for (int position = 1; position <= topPositionToDisplay; position++) {
             Button button = Button.byPosition(position);
             if (button != null) {
-                renderer.box(x, y, size, size, button.getColor(), button.getName(), Task.TextPosition.CENTER);
-                if (Lights.mouse.contains(x, y, size, size) && canInteract()) {
+                renderer.box(x, y, buttonWidth, buttonHeight, button.getColor(), button.getName(), Task.TextPosition.CENTER);
+                if (Lights.mouse.contains(x, y, buttonWidth, buttonHeight) && canInteract()) {
                     interacted = true;
                     if (Lights.mouse.leftClicked(1000)) {
                         button.press();
-                        renderer.box(x, y, size, size, new Color(button.getColor()).mul(1.5f));
+                        renderer.box(x, y, buttonWidth, buttonHeight, new Color(button.getColor()).mul(1.5f));
                     } else if (Lights.mouse.rightClicked()) {
                         buttonEditor.edit(button);
                     }
                 }
             } else {
-                renderer.box(x, y, size, size, Lights.color.DARK);
-                if (Lights.mouse.contains(x, y, size, size) && canInteract() && buttonEditor.isEditing()) {
+                renderer.box(x, y, buttonWidth, buttonHeight, Lights.color.DARK);
+                if (Lights.mouse.contains(x, y, buttonWidth, buttonHeight) && canInteract() && buttonEditor.isEditing()) {
                     if (Lights.mouse.leftClicked()) {
                         buttonEditor.getEditing().setPosition(position);
                     }
                 }
             }
-            x += size;
+            x += buttonWidth;
 
-            if (x + size > getX() + getWidth()) {
+            if (x + buttonWidth > getX() + width) {
                 x = getX();
-                y -= size;
+                y -= buttonHeight;
             }
         }
-
-        renderer.box(x, y, size, size, Lights.color.DARK, Lights.color.GREEN, "Add New", Task.TextPosition.CENTER);
-        if (Lights.mouse.contains(x, y, size, size) && canInteract()) {
-            interacted = true;
-            if (Lights.mouse.leftClicked(1000)) {
-                Button.add(new Button("New Button", Lights.color.MEDIUM));
-            }
-        }
-        y -= size;
-
-        setHeightBasedOnY(y);
         return interacted;
     }
 
